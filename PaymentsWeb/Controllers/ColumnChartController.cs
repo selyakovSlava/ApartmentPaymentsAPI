434b6c9869669be4b10517fa655f278a776e5f37
Vyacheslav Selyakov <v.selyakov@yandex.ru>
2024-08-29 19:58:00 +0300

    Добавлен график по платежам

---
+++ PaymentsWeb/Controllers/ColumnChartController.cs
@@ -0,0 +1,29 @@
+﻿using APModelsLibrary.Actions;
+using APModelsLibrary.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentsWeb.Controllers
+{
+    public class ColumnChartController : Controller
+    {
+        private readonly IAction _crudApi;
+
+        public ColumnChartController()
+        {
+            _crudApi = new PaymentActions();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public async Task<JsonResult> PaymentChart()
+        {
+            var payments = await _crudApi.GetAllAsync();
+            return Json(payments);
+        }
+    }
+}
